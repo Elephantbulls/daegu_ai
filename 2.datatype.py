d73c45833693dc6d25f982767775b4959a6a946e
Elephantbulls <102004325+Elephantbulls@users.noreply.github.com>
2022-03-28 15:46:29 +0900

    파이썬 첫 등장!

---
+++ 2.datatype.py
@@ -0,0 +1,32 @@
+print(1)
+print(1.1)
+print(1+1)
+print(2-1)
+print(2*2)
+print(6/2)
+print(pow(4,3))
+import random
+print(random.random())
+
+# String
+print('Hello')
+print("Hello")
+print('''
+    Hello
+    world
+''')
+print("""
+    Hello
+    world
+""")
+print(len('Hello'))
+a = 'hell world'
+print(a.replace('hell', 'hello'))
+# List
+member = ['egoing', 'duru', 'taeho']
+print(member[0])
+print(len(member))
+import random
+print(random.choice(member))
+score = [100,200,300]
+print(sum(score))
