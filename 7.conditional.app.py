b3041bb657844fe056ba962e01fc6585a9ab4759
Elephantbulls <102004325+Elephantbulls@users.noreply.github.com>
2022-03-29 15:28:21 +0900

    자바와 파이썬의 합동작전

---
+++ 7.conditional.app.py
@@ -0,0 +1,13 @@
+input_id = input('아이디가 뭔데요?')
+if input_id == 'silva' :
+    
+    print('hi')
+else:
+    print('누꼬?')
+
+input_pwd = input('비번이 뭔데요?')
+if input_pwd == '1234' :
+    
+    print('로그인 완료')
+else:
+    print('잡았다 도둑놈!')        
